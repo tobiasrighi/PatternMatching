9c2dc3f0b06d0fd91a6de09952425653994b3e29
Tobia <tobiasrighi@hotmail.com>
2017-03-13 13:23:00 -0700

    remove wrong app and include right one

---
+++ PatternMatching/WebApplication1/Models/LogEventModel.cs
@@ -0,0 +1,13 @@
+﻿using PatternLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class LogEventModel
+    {
+        public List<LogEvent> logEvents { get; set; }
+    }
+}
